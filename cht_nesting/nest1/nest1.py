2b2152185e4917bfa280c560e662fb2f261547e2
maartenvanormondt <82946105+maartenvanormondt@users.noreply.github.com>
2024-12-04 16:00:41 -0500

    added check for degrees east for sfincs_in_sfincs

--- cht_nesting/nest1/nest1.py
+++ cht_nesting/nest1/nest1.py
@@ -126,12 +126,20 @@ def nest1_beware_in_delft3dfm(overall, detail):
     
 def nest1_sfincs_in_sfincs(overall, detail):
     
-#    from sfincs import ObservationPoint as obspoint
-
     transformer = Transformer.from_crs(detail.crs,
                                        overall.crs,
                                        always_xy=True)
-    
+
+    # Check if overall model is in geographic coordinates. If so, make sure that longitude is correct. Either 0-360 or -180-180.
+    overall_degrees_west = False
+    if overall.crs.is_geographic:
+        # Get max longitude of the overall model
+        if overall.grid.data is None:
+            overall.grid.read()
+        x_max = overall.grid.data.grid.face_coordinates[:, 0].max()
+        if x_max > 180.0:
+            overall_degrees_west = True
+
     # Get list of names of the observation points
     overall_names = overall.observation_points.list_names()
     # Loop over all points in the gdf of the boundary conditions
@@ -143,15 +151,9 @@ def nest1_sfincs_in_sfincs(overall, detail):
         x = row["geometry"].coords[0][0]
         y = row["geometry"].coords[0][1]
         x, y = transformer.transform(x, y)
+        if x < 0 and overall_degrees_west:
+            x += 360
         overall.observation_points.add_point(x, y, name)
-    
-#     for ind, point in enumerate(detail.flow_boundary_point):
-
-#         name = detail.name + "_" + str(ind + 1).zfill(4)
-#         x, y = transformer.transform(point.geometry.x,
-#                                      point.geometry.y)
-# #        obs_list.append(obspoint(x, y, name, crs=overall.crs))
-#         overall.add_observation_point(x, y, name)
 
 def nest1_xbeach_in_sfincs(overall, detail):
     
